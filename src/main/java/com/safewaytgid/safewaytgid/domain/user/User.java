549a6222976c22bb175d1f29ae64578fd34ed21d
Igor Frutuoso <79484560+IgorFrutuoso@users.noreply.github.com>
2023-09-15 16:44:08 -0300

    implementação de validações com hibernate

--- src/main/java/com/safewaytgid/safewaytgid/domain/user/User.java
+++ src/main/java/com/safewaytgid/safewaytgid/domain/user/User.java
@@ -1,6 +1,10 @@
 package com.safewaytgid.safewaytgid.domain.user;
 
+import com.safewaytgid.safewaytgid.dtos.UserDTO;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -17,14 +21,31 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank(message = "Nome é obrigatio")
     private String name;
 
+    @NotBlank(message = "Cpf ou Cnpj é obrigatorio")
     @Column(unique = true)
     private String document;
 
+    @NotBlank(message = "Email é obrigatorio")
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull(message = "saldo é obrigatorio")
     private BigDecimal balance;
 
+    @NotNull(message = "Tipo de usuario é obrigatorio")
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.name = data.name();
+        this.document = data.document();
+        this.email = data.email();
+        this.balance = data.balance();
+        this.userType= data.userType();
+    }
 
 }
