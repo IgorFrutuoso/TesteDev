b4234c0ff9f653ca33e1808add0c640060aa15e2
Igor Frutuoso <79484560+IgorFrutuoso@users.noreply.github.com>
2023-09-15 11:08:05 -0300

    Primeira configuração

---
+++ src/main/java/com/safewaytgid/safewaytgid/SafewaytgidApplication.java
@@ -0,0 +1,13 @@
+package com.safewaytgid.safewaytgid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafewaytgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafewaytgidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safewaytgid/safewaytgid/SafewaytgidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safewaytgid.safewaytgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafewaytgidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
